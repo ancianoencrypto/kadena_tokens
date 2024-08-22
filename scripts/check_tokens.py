0976670088ed9970c090bface4f69827aba8a099
CryptoPascal31 <CryptoPascal31@users.noreply.github.com>
2024-08-22 15:40:42 +0200

    First test

---
+++ scripts/check_tokens.py
@@ -0,0 +1,55 @@
+import yaml
+from pathlib import Path
+
+import magic
+
+with open("../tokens.yaml") as fd:
+    data = yaml.safe_load(fd)
+
+mainnet = data["mainnet"]
+
+failure = False
+
+for token_mod, token in mainnet.items():
+    if "name" not in token:
+        print("{:s} - token name missing".format(token_mod))
+        failure = True
+        continue
+
+    token_name = token["name"]
+
+    if "symbol" not in token:
+        print("Token {:s} - symbol missing".format(token_name))
+        failure = True
+        continue
+
+    img = Path("..", token["img"])
+    if not img.exists():
+        print("Token {:s} - Image missing: {!s}".format(token_name, img))
+        failure = True
+        continue
+
+    img_type = magic.from_file(img, mime=True)
+    if not img_type.startswith("image"):
+        failure = True
+        print("Token {:s} - Invalid image: {!s}".format(token_name, img))
+
+    if "precision" not in token or not isinstance(token["precision"], int):
+        failure = True
+        print("Token {:s} - Precision invalid".format(token_name))
+
+    if "socials" not in token or not isinstance(token["socials"], list):
+        failure = True
+        print("Token {:s} - Invalid Socials".format(token_name))
+    else:
+        for soc_item in token["socials"]:
+            if soc_item.get("type", "") not in ["website", "github", "twitter", "discord", "telegram", "reddit"]:
+                print("Token {:s} - Invalid Social item: {!s}".format(token_name, soc_item))
+                failure = True
+            if "url" not in soc_item:
+                print("Token {:s} - Invalid Social item: {!s}".format(token_name, soc_item))
+
+                failure = True
+
+if not failure:
+    print("Token Database Valid")
